b969767e7a0c3754adafaf8a0b08fc396a9e2d13
iancaoo <33076510+iancaoo@users.noreply.github.com>
2017-11-02 21:37:01 -0500

    Initial Commit

---
+++ project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project2
+//
+//  Created by 曹旭宏 on 2017/11/2.
+//  Copyright © 2017年 xuhong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
